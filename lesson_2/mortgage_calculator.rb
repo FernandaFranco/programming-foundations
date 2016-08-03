9d8bf4c73a5e12368282f7e53f4030f740207675
Fernanda Franco <francodias.fernanda@gmail.com>
2016-08-03 19:02:28 -0400

    Applied code review suggestions

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -5,61 +5,85 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(n, i = 1)
-  case i
-  when 0, 2
-    /^\d+$/.match(n) && n.to_i > 0
-  when 1
-    /^\d*\.?\d+$/.match(n) && n.to_f.ceil > 0
-  end
+def valid_integer?(number)
+  /^\d+$/.match(number) && number.to_i > 0
+end
+
+def valid_float?(number)
+  /^\d*\.?\d+$/.match(number) && number.to_f > 0
+end
+
+def valid_answer?(string)
+  string.start_with?("n") || string.start_with?("y")
 end
 
 prompt(MESSAGES['welcome'])
 
-questions = ["the loan amount (in dollars)",
-             "the APR (in %)",
-             "the loan duration (in years)"]
-
 loop do # main loop
-  answers = []
-  questions.each_with_index do |string, index|
-    loop do
-      prompt(MESSAGES['choose'] + "#{string}?")
-      num = gets.chomp
-
-      if valid_number?(num, index)
-        answers.push(num)
-        break
-      else
-        prompt(MESSAGES['invalid'])
-      end
+  loan_amount = nil
+  loop do
+    prompt(MESSAGES['choose'] + "the loan amount (in dollars)?")
+    loan_amount = gets.chomp
+    if valid_integer?(loan_amount)
+      loan_amount = loan_amount.to_i
+      break
+    else
+      prompt(MESSAGES['invalid'])
     end
   end
 
-  loan = answers[0].to_i
-  annual_rate = answers[1].to_f
-  n_years = answers[2].to_i
+  annual_rate = nil
+  loop do
+    prompt(MESSAGES['choose'] + "the APR (in %)?")
+    annual_rate = gets.chomp
+    if valid_float?(annual_rate)
+      annual_rate = annual_rate.to_f
+      break
+    else
+      prompt(MESSAGES['invalid'])
+    end
+  end
+
+  n_years = nil
+  loop do
+    prompt(MESSAGES['choose'] + "the loan duration (in years)?")
+    n_years = gets.chomp
+    if valid_integer?(n_years)
+      n_years = n_years.to_i
+      break
+    else
+      prompt(MESSAGES['invalid'])
+    end
+  end
 
   prompt(MESSAGES['operating'])
-
   monthly_rate = (annual_rate / 12) / 100
   n_months = n_years * 12
-  monthly_pay = loan * (monthly_rate / (1 - (1 + monthly_rate)**-n_months))
+  monthly_pay = loan_amount *
+                (monthly_rate / (1 - (1 + monthly_rate)**-n_months))
 
-  unless valid_number?(monthly_pay.to_s)
+  unless valid_float?(monthly_pay.to_s)
     prompt(MESSAGES['error'])
     next
   end
 
-  result = monthly_pay
-  total_cost = result.round(2) * n_months
+  total_cost = monthly_pay.round(2) * n_months
 
-  prompt(MESSAGES['result'] + "#{format('%02.2f', result)} dollars.")
+  prompt(MESSAGES['result'] + "#{format('%02.2f', monthly_pay)} dollars.")
   prompt(MESSAGES['total'] + "#{format('%02.2f', total_cost)} dollars.")
 
-  prompt(MESSAGES['another'])
-  answer = gets.chomp
-  break if answer == "no"
+  answer = nil
+  loop do
+    prompt(MESSAGES['another'])
+    answer = gets.chomp.downcase
+    if valid_answer?(answer)
+      break
+    else
+      prompt(MESSAGES['invalid'])
+    end
+  end
+
+  break if answer.start_with?("n")
 end
 
 prompt(MESSAGES['thank_you'])
